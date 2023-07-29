88954e5b708b69bdea3d215dba25f7016c337995
Kirill F <kirill.olegovich31@gmail.com>
2023-07-29 17:45:27 +0300

    CRU for /menus is written and working

--- tests/test_menus.py
+++ tests/test_menus.py
@@ -17,7 +17,7 @@ def test_create_menu(session, client, PREFIX):
     res = client.post(f"{PREFIX}/menus", json=create_data)
     print("Test request was sent to", res.url)
     assert res.status_code == 201
-    created_menu = schemas.MenuCreate(**res.json())
+    created_menu = schemas.MenuOut(**res.json())
     assert created_menu.title == create_data["title"]
     assert created_menu.description == create_data["description"]
 
@@ -33,4 +33,26 @@ def test_get_menu(session, client, PREFIX, test_menus):
     assert response_menu.title == test_menus[0].title
 
 
-# Read
+# Read multiple testing
+def test_read_menus(session, client, PREFIX, test_menus):
+    res = client.get(f"{PREFIX}/menus")
+    response_data = res.json()
+    validated_menus_list = [schemas.MenuOut(**menu) for menu in response_data]
+
+    assert res.status_code == 200
+    assert len(validated_menus_list) == len(test_menus)
+
+
+# Update testing
+def test_update_menu(session, client, PREFIX, test_menus):
+    test_menu_id = test_menus[0].id
+    update_data = {
+        "title": "UPDATED test menu 1 title",
+        "description": "UPDATED test menu 1 description",
+    }
+    res = client.patch(f"{PREFIX}/menus/{test_menu_id}", json=update_data)
+    print("Test request was sent to", res.url)
+    assert res.status_code == 200
+    updated_menu = schemas.MenuCreate(**res.json())
+    assert updated_menu.title == update_data["title"]
+    assert updated_menu.description == update_data["description"]
