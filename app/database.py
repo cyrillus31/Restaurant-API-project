af464377feb7b5fd316acafd093843ad82d25da3
Kirill F <kirill.olegovich31@gmail.com>
2023-08-12 17:34:23 +0300

    read all menus workinig async

--- app/database.py
+++ app/database.py
@@ -24,3 +24,7 @@ async def get_session() -> AsyncSession:
     # finally:
         # db.close()
 
+
+async def init_db(engine):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from sqlalchemy_utils import create_database, database_exists
 
 from . import models
-from .database import engine, Base
+from .database import engine, Base, init_db
 from .routers import dishes, menus, submenus
 
 # create database
@@ -10,7 +10,7 @@ from .routers import dishes, menus, submenus
     # create_database(engine.url)
 
 # create tables
-# Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(engine)
 
 
 # # create database tables
@@ -24,6 +24,10 @@ app = FastAPI()
 app.include_router(menus.router)
 # app.include_router(submenus.router)
 # app.include_router(dishes.router)
+
+@app.on_event('startup')
+async def on_startup():
+    await init_db(engine)
 
 
 @app.get('/api/v1/')
